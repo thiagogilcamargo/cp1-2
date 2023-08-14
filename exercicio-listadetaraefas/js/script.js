f7301f9ee9922909fc5d8a2d14f28e6e7cc5cf09
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-08-14 02:52:29 -0300

    melhorias no codigo

--- exercicio-listadetaraefas/js/script.js
+++ exercicio-listadetaraefas/js/script.js
@@ -1,68 +1,64 @@
-// Array para armazenar as tarefas
-const tarefas = [];
+document.addEventListener('DOMContentLoaded', function() {
+    const tarefas = [];
 
-// Função para adicionar uma nova tarefa
-function adicionarTarefa() {
-    // Obtém os valores dos campos do formulário
-    const descricao = document.getElementById('descricao').value;
-    const autor = document.getElementById('autor').value;
-    const departamento = document.getElementById('departamento').value;
-    const importancia = parseInt(document.getElementById('importancia').value);
+    function adicionarTarefa() {
+        const descricao = document.getElementById('descricao').value;
+        const autor = document.getElementById('autor').value;
+        const departamento = document.getElementById('departamento').value;
+        const importancia = parseInt(document.getElementById('importancia').value);
 
-    // Validação dos campos do formulário
-    if (!descricao || !autor || !departamento || isNaN(importancia) || importancia < 1 || importancia > 5) {
-        alert('Preencha todos os campos corretamente. A importância deve ser um número de 1 a 5.');
-        return;
+        if (!descricao || !autor || !departamento || isNaN(importancia) || importancia < 1 || importancia > 5) {
+            alert('Preencha todos os campos corretamente. A importância deve ser um número de 1 a 5.');
+            return;
+        }
+
+        tarefas.push({ descricao, autor, departamento, importancia });
+
+        exibirTarefas();
+        limparCampos();
     }
 
-    // Adiciona a tarefa ao array de tarefas
-    tarefas.push({ descricao, autor, departamento, importancia });
+    function exibirTarefas() {
+        const listaTarefas = document.getElementById('tarefas');
+        listaTarefas.innerHTML = '';
 
-    // Atualiza a exibição das tarefas na tabela
+        tarefas.forEach((tarefa, index) => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${tarefa.descricao}</td>
+                <td>${tarefa.autor}</td>
+                <td>${tarefa.departamento}</td>
+                <td>${tarefa.importancia}</td>
+                <td><button onclick="removerTarefa(${index})">Remover</button></td>
+            `;
+            listaTarefas.appendChild(row);
+        });
+    }
+
+    function removerTarefa(index) {
+        tarefas.splice(index, 1);
+        exibirTarefas();
+    }
+
+    function ordenarPorImportancia() {
+        tarefas.sort((a, b) => b.importancia - a.importancia);
+        exibirTarefas();
+    }
+
+    function limparCampos() {
+        document.getElementById('descricao').value = '';
+        document.getElementById('autor').value = '';
+        document.getElementById('departamento').value = '';
+        document.getElementById('importancia').value = '';
+    }
+
+    // Configurar event listeners para botões
+    const adicionarBotao = document.getElementById('adicionarBotao');
+    adicionarBotao.addEventListener('click', adicionarTarefa);
+
+    const ordenarBotao = document.getElementById('ordenarBotao');
+    ordenarBotao.addEventListener('click', ordenarPorImportancia);
+
+    // Inicializa a exibição das tarefas
     exibirTarefas();
-
-    // Limpa os campos do formulário após adicionar uma tarefa
-    limparCampos();
-}
-
-// Função para exibir as tarefas na tabela
-function exibirTarefas() {
-    const listaTarefas = document.getElementById('tarefas');
-    listaTarefas.innerHTML = '';
-
-    // Percorre o array de tarefas e cria uma linha na tabela para cada tarefa
-    tarefas.forEach((tarefa, index) => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${tarefa.descricao}</td>
-            <td>${tarefa.autor}</td>
-            <td>${tarefa.departamento}</td>
-            <td>${tarefa.importancia}</td>
-            <td><button onclick="removerTarefa(${index})">Remover</button></td>
-        `;
-        listaTarefas.appendChild(row);
-    });
-}
-
-// Função para remover uma tarefa
-function removerTarefa(index) {
-    tarefas.splice(index, 1); // Remove a tarefa do array pelo índice
-    exibirTarefas(); // Atualiza a exibição das tarefas na tabela
-}
-
-// Função para ordenar as tarefas por importância (do maior para o menor)
-function ordenarPorImportancia() {
-    tarefas.sort((a, b) => b.importancia - a.importancia);
-    exibirTarefas();
-}
-
-// Função para limpar os campos do formulário
-function limparCampos() {
-    document.getElementById('descricao').value = '';
-    document.getElementById('autor').value = '';
-    document.getElementById('departamento').value = '';
-    document.getElementById('importancia').value = '';
-}
-
-// Inicializa a exibição das tarefas ao carregar a página
-exibirTarefas();
+});
